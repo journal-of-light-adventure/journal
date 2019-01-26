b5f70712ea040dc1bb9ab0a114dc1386896e9ae9
Rich McNeary <rlmcneary2@gmail.com>
2019-01-25 19:39:15 -0500

    Killington turkey

--- app.js
+++ app.js
@@ -786,6 +786,16 @@
         {
             date: "January 2019",
             description: [
+                "Vermont wildlife, seen in the parking lot. Luckily we opened the car door to be able to hear it calling."
+            ],
+            size: [1920, 1148],
+            src: "resources/killington-turkey.jpg",
+            tags: ["turkey", "vermont", "wildlife"],
+            title: "Birding in Vermont"
+        },
+        {
+            date: "January 2019",
+            description: [
                 "The beauty of the mountains. Can't wait to ski - tomorrow."
             ],
             size: [1920, 1280],
